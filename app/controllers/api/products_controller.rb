6c87782c93e1361bce51268bce492cb1df343c25
Peter Jang <peter.x.jang@gmail.com>
2018-10-11 21:13:27 -0500

    Add products action to see first product

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -2,5 +2,10 @@ class Api::ProductsController < ApplicationController
   def all_products_method
     @products = Product.all
     render "all_products.json.jbuilder"
+  end  
+
+  def first_product_method
+    @product = Product.first
+    render "first_product.json.jbuilder"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     get "/all_products_url" => "products#all_products_method"
+    get "/first_product_url" => "products#first_product_method"
   end
 end
