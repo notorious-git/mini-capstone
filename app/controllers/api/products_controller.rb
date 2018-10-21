078f75a65a20d720dcf10a49c3b356c2fd6cf14a
Peter Jang <peter.x.jang@gmail.com>
2018-10-21 09:56:28 -0500

    Fix missing save in update action

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -41,6 +41,7 @@ class Api::ProductsController < ApplicationController
     @product.price = params["price"] || @product.price
     @product.image_url = params["image_url"] || @product.image_url
     @product.description = params["description"] || @product.name
+    @product.save
     render "show.json.jbuilder"
   end
 
