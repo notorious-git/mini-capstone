cf2d56186c4426de35b6779a6843983736b28e23
Peter Jang <peter.x.jang@gmail.com>
2018-10-22 21:32:18 -0500

    Change product to have multiple images, associate product with images

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,11 @@ class Product < ApplicationRecord
   validates :price, numericality: { greater_than: 0 }
   validates :description, length: { in: 10..5000 }
 
+  has_many :images
+  # def images
+  #   Image.where(product_id: id)
+  # end
+
   def is_discounted?
     price < 50
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_23_002935) do
+ActiveRecord::Schema.define(version: 2018_10_23_021459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "images", force: :cascade do |t|
+    t.string "url"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.decimal "price", precision: 9, scale: 2
-    t.string "image_url"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
