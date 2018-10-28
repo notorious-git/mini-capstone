fcc4141bb64b21ed8f2e110ffba33632d1690192
Peter Jang <peter.x.jang@gmail.com>
2018-10-28 09:12:50 -0500

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,25 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-supplier = Supplier.new(name: "Amazon", email: "amazon@email.com", phone_number: "234234324")
-supplier.save
-supplier = Supplier.new(name: "ThinkGeek", email: "thinkgeek@email.com", phone_number: "134234324")
-supplier.save
+User.create!(name: "Peter", email: "peter@email.com", password: "password", password_confirmation: "password")
+User.create!(name: "Test", email: "test@email.com", password: "password", password_confirmation: "password")
 
-product = Product.new({supplier_id: 1, name: "WNYX Mug", price: 2, description: "Get your morning news once you wake up with a cup of joe from... well Joe. He made it with his homemade duct tape"})
-product.save!
-product = Product.new({supplier_id: 1, name: "Hitchhiker's Guide to the Galaxy", price: 42, description: "In many of the more relaxed civilizations on the Outer Eastern Rim of the Galaxy, the Hitch-Hiker's Guide has already supplanted the great Encyclopaedia Galactica as the standard repository of all knowledge and wisdom, for though it has many omissions and contains much that is apocryphal, or at least wildly inaccurate, it scores over the older, more pedestrian work in two important respects. First, it is slightly cheaper; and secondly it has the words DON'T PANIC inscribed in large friendly letters on its cover."})
-product.save!
-product = Product.new({supplier_id: 2, name: "Lightsaber", price: 270, description: "Part laser, part samuri sword, all awesome. The lightsaber is an elogant weapon for a more civilized age, not nearly as clumsy as a blaster"})
-product.save!
-product = Product.new({supplier_id: 1, name: "Space Cowboy Laser Gun", price: 170, description: "This weapon has no other description than, Shiney!"})
-product.save!
-product = Product.new({supplier_id: 2, name: "DnD Dice set", price: 57, description: "Take down mighty dragons with this timeless set of 20 sided wonders"})
-product.save!
-product = Product.new({supplier_id: 1, name: "Sonic Screwdriver", price: 9, description: "The Doctor's sciencey magic wand to get out of tight spots. Note: does not work on wood"})
-product.save!
-product = Product.new({supplier_id: 1, name: "Yoda sleeping bag", price: 40, description: "For real, this is for real"})
-product.save!
+Supplier.create!(name: "Amazon", email: "amazon@email.com", phone_number: "234234324")
+Supplier.create!(name: "ThinkGeek", email: "thinkgeek@email.com", phone_number: "134234324")
+
+Product.create!(supplier_id: 1, name: "WNYX Mug", price: 2, description: "Get your morning news once you wake up with a cup of joe from... well Joe. He made it with his homemade duct tape")
+Product.create!(supplier_id: 1, name: "Hitchhiker's Guide to the Galaxy", price: 42, description: "In many of the more relaxed civilizations on the Outer Eastern Rim of the Galaxy, the Hitch-Hiker's Guide has already supplanted the great Encyclopaedia Galactica as the standard repository of all knowledge and wisdom, for though it has many omissions and contains much that is apocryphal, or at least wildly inaccurate, it scores over the older, more pedestrian work in two important respects. First, it is slightly cheaper; and secondly it has the words DON'T PANIC inscribed in large friendly letters on its cover.")
+Product.create!(supplier_id: 2, name: "Lightsaber", price: 270, description: "Part laser, part samuri sword, all awesome. The lightsaber is an elogant weapon for a more civilized age, not nearly as clumsy as a blaster")
+Product.create!(supplier_id: 1, name: "Space Cowboy Laser Gun", price: 170, description: "This weapon has no other description than, Shiney!")
+Product.create!(supplier_id: 2, name: "DnD Dice set", price: 57, description: "Take down mighty dragons with this timeless set of 20 sided wonders")
+Product.create!(supplier_id: 1, name: "Sonic Screwdriver", price: 9, description: "The Doctor's sciencey magic wand to get out of tight spots. Note: does not work on wood")
+Product.create!(supplier_id: 1, name: "Yoda sleeping bag", price: 40, description: "For real, this is for real")
 
 Image.create!(product_id: 1, url: "https://i.pinimg.com/236x/14/4a/0b/144a0bffe37f29f414e2dae2bd0141cf.jpg")
 Image.create!(product_id: 2, url: "http://www.notcot.com/images/guide.gif")
@@ -34,3 +28,5 @@ Image.create!(product_id: 4, url: "http://cdn.shopify.com/s/files/1/0289/1534/pr
 Image.create!(product_id: 5, url: "https://s-media-cache-ak0.pinimg.com/736x/9c/15/7b/9c157bea5331463f9c539cbce739a4b8.jpg")
 Image.create!(product_id: 6, url: "https://dyn0.media.forbiddenplanet.com/products/28577492.jpg")
 Image.create!(product_id: 7, url: "https://staticdelivery.nexusmods.com/mods/1151/images/12353-0-1461721930.png")
+
+Order.create!(user_id: 1, product_id: 2, quantity: 10, subtotal: 420, tax: 37.80, total: 457.80)
