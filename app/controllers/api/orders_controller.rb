840a526b4493d5e11c896ac9e15b0af408b5cdb5
Peter Jang <peter.x.jang@gmail.com>
2018-10-30 20:10:38 -0500

    Update the orders create action to use shopping cart

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -2,22 +2,37 @@ class Api::OrdersController < ApplicationController
   before_action :authenticate_user
 
   def index
-    @orders = current_user.orders
-    render "index.json.jbuilder"
+    # @orders = current_user.orders
+    # render "index.json.jbuilder"
   end
 
   def create
-    product = Product.find_by(id: params[:product_id])
+    calculated_subtotal = 0
+    calculated_tax = 0
+    calculated_total = 0
+    carted_products = current_user.carted_products.where(status: "carted")
+    carted_products.each do |carted_product|
+      calculated_subtotal += carted_product.product.price * carted_product.quantity
+      calculated_tax += carted_product.product.tax * carted_product.quantity
+    end
+    calculated_total = calculated_subtotal + calculated_tax
 
     @order = Order.new(
-      product_id: params[:product_id],
-      quantity: params[:quantity],
       user_id: current_user.id,
-      subtotal: product.price * params[:quantity].to_i,
-      tax: product.tax * params[:quantity].to_i,
-      total: product.total * params[:quantity].to_i
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total
     )
     @order.save
+
+    # carted_products.each do |carted_product|
+    #   carted_product.status = "purchased"
+    #   carted_product.order_id = @order.id
+    #   carted_product.save
+    # end
+    carted_products.update_all(status: "purchased", order_id: @order.id)
+
+
     render "show.json.jbuilder"
   end
 end
--- app/views/api/orders/show.json.jbuilder
+++ app/views/api/orders/show.json.jbuilder
@@ -1,6 +1,5 @@
 json.id @order.id
-json.product_id @order.product_id
-json.quantity @order.quantity
+json.carted_products @order.carted_products
 json.subtotal @order.subtotal
 json.tax @order.tax
 json.total @order.total
