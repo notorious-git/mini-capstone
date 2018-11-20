ee8592102dff7b563c78890c91acc1acd117e454
Peter Jang <peter.x.jang@gmail.com>
2018-11-20 13:15:13 -0600

    Refactor logic

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -7,32 +7,13 @@ class Api::OrdersController < ApplicationController
   end
 
   def create
-    calculated_subtotal = 0
-    calculated_tax = 0
-    calculated_total = 0
-    carted_products = current_user.carted_products.where(status: "carted")
-    carted_products.each do |carted_product|
-      calculated_subtotal += carted_product.product.price * carted_product.quantity
-      calculated_tax += carted_product.product.tax * carted_product.quantity
-    end
-    calculated_total = calculated_subtotal + calculated_tax
-
     @order = Order.new(
-      user_id: current_user.id,
-      subtotal: calculated_subtotal,
-      tax: calculated_tax,
-      total: calculated_total
+      user_id: current_user.id
     )
     @order.save
-
-    # carted_products.each do |carted_product|
-    #   carted_product.status = "purchased"
-    #   carted_product.order_id = @order.id
-    #   carted_product.save
-    # end
+    carted_products = current_user.carted_products.where(status: "carted")
     carted_products.update_all(status: "purchased", order_id: @order.id)
-
-
+    @order.calculate_money
     render "show.json.jbuilder"
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,16 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :carted_products
   has_many :products, through: :carted_products
+
+  def calculate_money
+    calculated_subtotal = 0
+    calculated_tax = 0
+    calculated_total = 0
+    self.carted_products.each do |carted_product|
+      calculated_subtotal += carted_product.product.price * carted_product.quantity
+      calculated_tax += carted_product.product.tax * carted_product.quantity
+    end
+    calculated_total = calculated_subtotal + calculated_tax
+    self.update(subtotal: calculated_subtotal, tax: calculated_tax, total: calculated_total)
+  end
 end
